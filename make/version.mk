c62f081bc68e11a3a626e4d334afef46f721ec6a
Yuriy Olkhovyy <y.olkhovyy@gmail.com>
2025-03-30 08:43:53 +0200

    Fix APP_VERSION

--- make/version.mk
+++ make/version.mk
@@ -1,7 +1,6 @@
-
 export GIT_TAG = $(shell git describe --tags --abbrev=0 2>/dev/null || echo "v0.0.0")
 
-ifeq ($(shell git diff-index --quiet HEAD --; echo $$?), 1)
+ifneq ($(shell git rev-list $(GIT_TAG)..HEAD --count), 0)
     export TIMESTAMP = $(shell date +%Y%m%d%H%M%S)
     export APP_VERSION = $(GIT_TAG)-$(TIMESTAMP)
 else
